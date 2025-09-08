937caa695edf5e645a2c7efe03b09915d36c12b3
Naman <namanjaju2004@gmail.com>
2025-09-09 01:23:52 +0530

    Auth file structure setup

---
+++ src/app/(auth)/auth/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from 'next/image'
+
+const SignInPage: React.FC = () => {
+  return (
+    <>
+      <Image src="/public/Logo.svg" alt="Logo" width={300} height={300} />
+    </>
+  )
+}
+
+export default SignInPage
